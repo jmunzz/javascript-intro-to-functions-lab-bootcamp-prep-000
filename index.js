a6c9805a80ecd74776c5101970fdc94a28451a3b
Jeb Henrie <jebhenrie@gmail.com>
2018-07-26 13:48:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string){
+} 
+function shout(string){
+  return(string) 
+}
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return (string)
+}
+function whisper(string){
+  return string.toLowerCase()
+}
