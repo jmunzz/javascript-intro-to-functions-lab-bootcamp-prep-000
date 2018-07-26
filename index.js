e101b1341e2bf3a1c9bcfb64bd51249d80f70d60
Jeb Henrie <jebhenrie@gmail.com>
2018-07-26 16:45:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,4 +27,10 @@ function sayHiToGrandma(string)
   else if (string.toUpperCase()===
   string){
     return "YES INDEED!"
-  } 
+  }
+  else (string.toUpperCase()===
+  "I love you, Grandma"){
+    return "I love you, too" }
+}
+
+  
